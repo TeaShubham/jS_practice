98abbe8398eb97e0cf0cc61c73c56dc3a6ff9ee3
TeaShubham <thangeshubham7276@gmail.com>
2025-03-11 15:16:44 +0530

    added localstorage and json

--- game.js
+++ game.js
@@ -1,65 +1,124 @@
-            const Score={
-                wins:0,
-                losses:0,
-                tied:0
-            };
-            function Reset_Score() {
-            Score.wins=0;
-            Score.losses=0;
-            Score.tied=0;
-            }
-            
-            function playGame(playerMove){
-                        const computerMove=ComputerMove();
-                        let result='';
-                        if(playerMove === 'rock'){
-                        if (computerMove === 'rock') {
-                            result='tied';
-                        } else if(computerMove === 'paper'){
-                            result='lost';
-                        } else if(computerMove === 'scissor'){
-                            result='won';
-                        }
-                    }else if(playerMove === 'paper'){
-                        if (computerMove === 'paper') {
-                            result='tied';
-                        } else if(computerMove === 'scissor'){
-                            result='lost';
-                        } else if(computerMove === 'rock'){
-                            result='won';
-                        }
-                    }
-                    else if(playerMove === 'scissor'){
-                        if (computerMove === 'paper') {
-                            result='won';
-                        } else if(computerMove === 'scissor'){
-                            result='tied';
-                        } else if(computerMove === 'rock'){
-                            result='lost';
-                        }
-                    }
-            if (result === 'won') {
-                Score.wins +=1;
-            }else if(result === 'lost'){
-                Score.losses +=1;
-            }else if(result === 'tied'){
-                Score.tied +=1;
-            }
-             alert(`you picked ${playerMove} and computer pick is ${computerMove}, you ${result}
-            wins: ${Score.wins}, Losses: ${Score.losses}, tied: ${Score.tied}`);
-            }
-                    
-            
-                    function ComputerMove() {
-                       let randomNumber=Math.random();
-                       let ComputerMove='';
-                       if (randomNumber>0 && randomNumber<1/3) {
-                        ComputerMove='rock';
-                        
-                       } else if(randomNumber > 1/3 && randomNumber<2/3) {
-                        ComputerMove='paper';
-                       } else if(randomNumber>2/3 && randomNumber <1){
-                        ComputerMove='scissor';
-                       }
-                       return ComputerMove;
-                    }
+let score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    losses: 0,
+    ties: 0
+  };
+  
+  updateScoreElement();
+  
+  let isAutoPlaying = false;
+  let intervalId;
+  
+ 
+  function autoPlay() {
+    if (!isAutoPlaying) {
+      intervalId = setInterval(() => {
+        const playerMove = pickComputerMove();
+        playGame(playerMove);
+      }, 1000);
+      isAutoPlaying = true;
+  
+    } else {
+      clearInterval(intervalId);
+      isAutoPlaying = false;
+    }
+  }
+  
+  document.querySelector('.js-rock-button')
+    .addEventListener('click', () => {
+      playGame('rock');
+    });
+  
+  document.querySelector('.js-paper-button')
+    .addEventListener('click', () => {
+      playGame('paper');
+    });
+  
+  document.querySelector('.js-scissors-button')
+    .addEventListener('click', () => {
+      playGame('scissors');
+    });
+  
+  document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'r') {
+      playGame('rock');
+    } else if (event.key === 'p') {
+      playGame('paper');
+    } else if (event.key === 's') {
+      playGame('scissors');
+    }
+  });
+  
+  function playGame(playerMove) {
+    const computerMove = pickComputerMove();
+  
+    let result = '';
+  
+    if (playerMove === 'scissors') {
+      if (computerMove === 'rock') {
+        result = 'You lose.';
+      } else if (computerMove === 'paper') {
+        result = 'You win.';
+      } else if (computerMove === 'scissors') {
+        result = 'Tie.';
+      }
+  
+    } else if (playerMove === 'paper') {
+      if (computerMove === 'rock') {
+        result = 'You win.';
+      } else if (computerMove === 'paper') {
+        result = 'Tie.';
+      } else if (computerMove === 'scissors') {
+        result = 'You lose.';
+      }
+      
+    } else if (playerMove === 'rock') {
+      if (computerMove === 'rock') {
+        result = 'Tie.';
+      } else if (computerMove === 'paper') {
+        result = 'You lose.';
+      } else if (computerMove === 'scissors') {
+        result = 'You win.';
+      }
+    }
+  
+    if (result === 'You win.') {
+      score.wins += 1;
+    } else if (result === 'You lose.') {
+      score.losses += 1;
+    } else if (result === 'Tie.') {
+      score.ties += 1;
+    }
+  
+    localStorage.setItem('score', JSON.stringify(score));
+  
+    updateScoreElement();
+  
+    document.querySelector('.js-result').innerHTML = result;
+  
+    document.querySelector('.js-moves').innerHTML = `You
+  <img src="assets/${playerMove}-emoji.png" class="move-icon">
+  <img src="assets/${computerMove}-emoji.png" class="move-icon">
+  Computer`;
+  }
+  
+  function updateScoreElement() {
+    document.querySelector('.js-score')
+      .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+  }
+  
+  function pickComputerMove() {
+    const randomNumber = Math.random();
+  
+    let computerMove = '';
+  
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+      computerMove = 'rock';
+    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+      computerMove = 'paper';
+    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+      computerMove = 'scissors';
+    }
+  
+    return computerMove;
+  }
